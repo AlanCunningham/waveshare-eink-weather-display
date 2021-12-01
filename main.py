da9a5ea43db96028d37c76782e23c78afb9b367e
Alan Cunningham <ajc.alan@gmail.com>
2021-12-01 19:46:24 +0000

    Use hourly rain chance and temperature for "current" values
    This should then match up with the same value in the graphs

--- main.py
+++ main.py
@@ -97,7 +97,6 @@ def main():
         hour_list.append(converted_time)
         rain_chance_list.append(hour["precipProbability"] * 100)
         cloud_cover_list.append(hour["cloudCover"] * 100)
-        print(f"{hour}: {hour['precipProbability']}")
     # Plot the graph, save it as an image and add to the eink buffer
     plt.figure(figsize=(graphs_width, graphs_height))
     plt.grid()
@@ -119,7 +118,7 @@ def main():
     # Images
     image_dir = "climacons"
     # If chance of rain is 100%, shift the icon and text over to the left slightly
-    rain_chance = int(current_weather["precipProbability"] * 100)
+    rain_chance = int(hourly_weather[0]["precipProbability"] * 100)
     if rain_chance == 100:
         umbrella_icon_x = max_left + 40
         rain_chance_x = umbrella_icon_x + 70
@@ -136,8 +135,7 @@ def main():
     draw_underlined_text(draw, (todays_date_x, todays_date_y), todays_date, font=normal, width=4)
     # draw.text((todays_date_x, todays_date_y), todays_date, font=normal)
     # Chance of rain
-    rain_chance = int(round(current_weather["precipProbability"] * 100))
-    print(f"Rain chance: {current_weather['precipProbability']}")
+    rain_chance = int(round(hourly_weather[0]["precipProbability"] * 100))
     draw.text((rain_chance_x, rain_chance_y), f"{rain_chance}%", font=large)
     # Weather icon
     # Convert 200x200 svg images to png, resized to around 100x100.
@@ -148,7 +146,7 @@ def main():
     except Exception:
         draw.text((weather_icon_x, weather_icon_y), f"Image {current_weather_icon} not found", font=normal)
     # Current Temperature
-    current_temperature = str(int(round(current_weather["temperature"])))
+    current_temperature = str(int(round(hourly_weather[0]["temperature"])))
     draw.text((current_temperature_x, current_temperature_y), f"{current_temperature}°", font=large)
     # Weather summary
     # draw.text((summary_x, summary_y),  weather_data["hourly"]["summary"], font=normal)
